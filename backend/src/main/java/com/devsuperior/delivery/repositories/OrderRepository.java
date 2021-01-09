6ce155a74576eee5e3f9dbf2b51c0be8b2de3dd3
Henrique Nawa <henrique.nawa@fatec.sp.gov.br>
2021-01-09 14:42:02 -0300

    Get /products

---
+++ backend/src/main/java/com/devsuperior/delivery/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.devsuperior.delivery.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.delivery.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
---
+++ backend/src/main/java/com/devsuperior/delivery/repositories/ProductRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.devsuperior.delivery.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.delivery.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	List<Product> findAllByOrderByNameAsc();
+	
+}
